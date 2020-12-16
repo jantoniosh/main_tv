95d8983bef3aabecc95da4a5fea1b7ab6738b917
Antonio Salinas <salinash.jantonio@gmail.com>
2020-12-16 12:19:59 -0600

    Archivos Build React App

---
+++ docs/precache-manifest.a648fb826ea804ad43f0856a208d41ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7683415374b4831aca000b79c71527e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf79c1af0769dea1fbfa",
+    "url": "/static/css/main.f2597f1e.chunk.css"
+  },
+  {
+    "revision": "265223d414e465ba637f",
+    "url": "/static/js/2.7d3ab822.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.7d3ab822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf79c1af0769dea1fbfa",
+    "url": "/static/js/main.2f72c488.chunk.js"
+  },
+  {
+    "revision": "ba916d8ba2a130768c87",
+    "url": "/static/js/runtime-main.f3bfd706.js"
+  }
+]);
